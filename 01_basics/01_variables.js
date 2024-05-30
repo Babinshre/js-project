9a9aeb08f328d5cb7cdb987ea0b20c812368454f
babinshre <babinshrestha5748@gmail.com>
2024-05-30 13:41:01 +0545

    variable and datatypes class

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const userId = "1"
+let userName = "babin"
+var userCity = "ktm" /*this type of datatypes creates issues in blocks so not preffered*/
+userSex = "male"/*so not preffered*/
+console.log([userId,userName,userCity,userSex])
+/*this is comment note section*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name = "babin"
+let age = 27
+let isLoggedIn = false
+
+// types of datatypes
+/*
+1.number => 2 to pow 53
+2.bigint
+3.string => ""
+4.boolean => true/false
+5.null => standalone value
+6.undefined
+7.symbol => unique
+
+*/
+
+console.log(typeof undefined) // typeof is used to check the dataypes 
+console.log(typeof null) // null is object
