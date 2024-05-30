978e0ba22b54d7036472154438941c8436854059
babinshre <babinshrestha5748@gmail.com>
2024-05-30 14:12:34 +0545

    datatypes conversion class

---
+++ 01_basics/03_datatypesconversion.js
@@ -0,0 +1,21 @@
+/* convert number to string */
+
+/*
+let userId = 5
+let valueId = String(userId)
+console.log(valueId);
+console.log(typeof valueId);
+
+*/
+
+/* cannot convert string to number */
+let userName = "babin"
+let valueId = Number(userName)
+console.log(valueId);
+console.log(typeof valueId);
+
+/* convert string to boolean */
+let userClass = "upper"
+let result = Boolean(userClass)
+console.log(result);
+console.log(typeof result);
