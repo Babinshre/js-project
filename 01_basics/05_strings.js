4d56de020738265a1ada2cf04f92e983892cd2af
babinshre <babinshrestha5748@gmail.com>
2024-06-05 16:14:09 +0545

    more of strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+
+// working with string 
+
+let str1 = "babinpal"
+let newStr = str1.substring(-5,3) //ignores  negative value and count it as a 0
+//let newStr2 = str1.slice(-8,4) // Negative value start with end 
+console.log(newStr);
+const url = "wwww.babinshrestha@@.com"
+console.log(url.replace('@@','99'));
