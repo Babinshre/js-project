0274d422ea15384624d2765b55692c389bcb32f3
babinshre <babinshrestha5748@gmail.com>
2024-06-07 22:09:53 +0545

    dates in js class

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+console.log(myDate.toLocaleString());
+console.log(myDate.getMonth()+1);
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString())
+
+
+// custom date
+
+let myCustDate  = new Date("2024-01-05")
+console.log(myCustDate);
+console.log(myCustDate);
+console.log(myCustDate.toLocaleString());
+
+// current date
+
+let myCdate  = Date.now()
+console.log(myCdate);
+console.log(myCustDate.getTime());
